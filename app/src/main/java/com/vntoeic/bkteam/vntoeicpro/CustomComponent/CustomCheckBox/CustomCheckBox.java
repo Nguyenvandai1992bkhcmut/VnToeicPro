6a193ada672e05f5eb3af16f888ea5b6968ffc9d
Tran Thi Cam Giang <1410986@hcmut.edu.vn>
2017-06-19 02:10:56 +0700

    commit example

---
+++ app/src/main/java/com/vntoeic/bkteam/vntoeicpro/CustomComponent/CustomCheckBox/CustomCheckBox.java
@@ -0,0 +1,173 @@
+package com.vntoeic.bkteam.vntoeicpro.CustomComponent.CustomCheckBox;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.vntoeic.bkteam.vntoeicpro.R;
+
+/**
+ * Created by giang on 6/2/17.
+ */
+
+public class CustomCheckBox extends RelativeLayout implements Checkable{
+
+    private View mRootView;
+    private ImageView mImageView;
+    private TextView mTextView;
+
+    private ECheckable mState;
+    private boolean isShow;
+    private boolean isAnswer;
+    private boolean isSol = false;
+    private int mId = 0;
+    private CheckBoxController mController = null;
+
+    private final int RES_CHECKED = R.mipmap.icon_checked;
+    private final int RES_NOTCHECKED = R.mipmap.icon_notchecked;
+    private final int RES_TRUE = R.mipmap.icon_true;
+    private final int RES_FALSE = R.mipmap.icon_false;
+
+    public boolean isAnswer() {
+        return isAnswer;
+    }
+
+    public ECheckable getState() {
+        return mState;
+    }
+
+    public void setState(ECheckable state) {
+        if (isShow && (state == ECheckable.TRUE || state == ECheckable.FALSE))
+            setCheck(state);
+        else if (!isShow && (state == ECheckable.CHECKED || state == ECheckable.NOT_CHECKED))
+            setCheck(state);
+        else
+            setCheck(ECheckable.NOT_CHECKED);
+    }
+
+    public boolean isShow() {
+        return isShow;
+    }
+
+    public void setAnswer(boolean answer) {
+        isAnswer = answer;
+
+    }
+
+    public CustomCheckBox(Context context, boolean isSol, int id, CheckBoxController controller) {
+        super(context);
+        init(context, isSol, id, controller);
+        controller.add(this);
+    }
+
+
+
+    public CustomCheckBox(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public CustomCheckBox(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+
+
+    private void init(Context context, boolean isSol, int id, CheckBoxController controller) {
+
+        init(context);
+        this.isSol = isSol;
+        this.mId = id;
+        this.mController = controller;
+    }
+
+    private void init(Context context) {
+        mRootView = inflate(context, R.layout.custom_checkbox, this);
+        mRootView.setBackgroundColor(getResources().getColor(R.color.gray_green));
+        mImageView = (ImageView) mRootView.findViewById(R.id.imageView);
+        mTextView = (TextView) mRootView.findViewById(R.id.textView);
+        setState(ECheckable.NOT_CHECKED);
+        isShow = false;
+        isAnswer = false;
+
+        mRootView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggle();
+            }
+        });
+    }
+
+    public void setText(String s) {
+        mTextView.setText(s);
+    }
+
+    private void onStateChanged() {
+        setDrawable(getDrawable());
+    }
+
+    public int getDrawable() {
+
+        switch (mState) {
+            case CHECKED: {
+                return RES_CHECKED;
+            }
+
+            case TRUE: {
+                return RES_TRUE;
+            }
+            case FALSE: {
+                return RES_FALSE;
+            }
+            default:
+                return RES_NOTCHECKED;
+
+        }
+    }
+
+    private void setDrawable(int resId) {
+        mImageView.setImageResource(resId);
+    }
+
+
+
+
+    @Override
+    public void setCheck(ECheckable eCheckable) {
+        mState = eCheckable;
+        onStateChanged();
+    }
+
+    @Override
+    public ECheckable getCheck() {
+        return mState;
+    }
+
+    @Override
+    public void toggle() {
+        switch (mState){
+            case CHECKED:{
+//                setState(ECheckable.NOT_CHECKED);
+                break;
+            }
+            case NOT_CHECKED:{
+                setState(ECheckable.CHECKED);
+                mController.itemOnChangeState(mId);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void setShow(boolean isShow) {
+        this.isShow = isShow;
+    }
+}
---
+++ app/src/main/java/com/vntoeic/bkteam/vntoeicpro/Reading/Fragment/QuestionFragment.java
@@ -0,0 +1,93 @@
+package com.vntoeic.bkteam.vntoeicpro.Reading.Fragment;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.vntoeic.bkteam.vntoeicpro.CustomComponent.CustomCheckBox.CheckBoxController;
+import com.vntoeic.bkteam.vntoeicpro.CustomComponent.CustomCheckBox.CustomCheckBox;
+import com.vntoeic.bkteam.vntoeicpro.R;
+
+import model.EAnswer;
+import model.ModelPart7Question;
+
+/**
+ * Created by giang on 6/2/17.
+ */
+
+public class QuestionFragment extends Fragment {
+    private static final String MODEL_PART7_QUESTION = "model_part7_question";
+    private static QuestionFragment mInstance;
+    private ModelPart7Question mQuestion;
+
+
+    private TextView mQuestionTxt;
+    private CustomCheckBox mA, mB, mC, mD;
+    private Context mContext;
+    private CheckBoxController mController;
+
+
+    @Override
+    public void onAttach(Context context) {
+        this.mContext = context;
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_question, container, false);
+
+        getData();
+        bindView(view);
+        return view;
+    }
+
+    private void bindView(View view) {
+        mController = new CheckBoxController();
+        mQuestionTxt = (TextView) view.findViewById(R.id.textView);
+        mQuestionTxt.setText(mQuestion.getQuestion());
+        LinearLayout questionLayout = (LinearLayout) view.findViewById(R.id.question_layout);
+        mA = initQuestion(mQuestion.getA(), mQuestion.getSol() == EAnswer.A, 0, questionLayout);
+        mB = initQuestion(mQuestion.getB(), mQuestion.getSol() == EAnswer.B, 1, questionLayout);
+        mC = initQuestion(mQuestion.getC(), mQuestion.getSol() == EAnswer.C, 2, questionLayout);
+        mD = initQuestion(mQuestion.getD(), mQuestion.getSol() == EAnswer.D, 3, questionLayout);
+    }
+
+    private CustomCheckBox initQuestion(String s, boolean isSol, int id, LinearLayout questionLayout) {
+        CustomCheckBox checkBox = new CustomCheckBox(mContext, isSol, id, mController);
+        checkBox.setText(s);
+        questionLayout.addView(checkBox, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        return checkBox;
+    }
+
+    private void getData() {
+        Bundle bundle = getArguments();
+        mQuestion = (ModelPart7Question) bundle.getSerializable(MODEL_PART7_QUESTION);
+    }
+
+    public static QuestionFragment getInstance(ModelPart7Question question) {
+
+        if (mInstance == null) {
+
+        }
+
+        mInstance = new QuestionFragment();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(MODEL_PART7_QUESTION, question);
+        mInstance.setArguments(bundle);
+        return mInstance;
+    }
+
+    public void animate(View view) {
+
+    }
+}
